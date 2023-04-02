fc1550a40e7622b9c7d74aa1327ed01c53372520
guantanamobosch <luketyson17@gmail.com>
2023-04-01 19:07:28 -0600

    register models

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('boards/', views.boards_index, name='index'),
-    path('posts/<int:post_id>', views.post_detail, name='detail')
+    # path('boards/', views.boards_index, name='index'),
+    # path('posts/<int:post_id>', views.post_detail, name='detail')
 ]
