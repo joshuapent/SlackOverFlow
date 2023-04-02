083a8b3d1f7786b28c677bc3055f60af10a4cc41
guantanamobosch <luketyson17@gmail.com>
2023-04-01 18:12:58 -0600

    setting up user auth

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, 'login.html')
+
+def signup()
+
 def home(request):
     return render(request, 'home.html')
 
