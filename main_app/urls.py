0869a14676b4fa37b28fd3fa1102961462085748
joshuapent <joshdavidpent@gmail.com>
2023-04-02 22:03:57 -0500

    Added a bunch of changes and updates

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('boards/', views.boards_index, name='index'),
+    path('forum/', views.main_forum, name='main_forum'),
+    path('class-forum/', views.class_forum, name='class_forum'),
+    path('boards/', views.boards_index, name='index'),
     # path('posts/<int:post_id>', views.post_detail, name='detail')
 ]
