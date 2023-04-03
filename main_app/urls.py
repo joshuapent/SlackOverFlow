a6a5c8b76da9cdc17a30941157b1a205a77f00a1
joshuapent <joshdavidpent@gmail.com>
2023-04-03 08:55:29 -0500

    Added some changes to urls and views.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('accounts/register/', views.register, name='register'),
     path('forum/', views.main_forum, name='main_forum'),
     path('class-forum/', views.class_forum, name='class_forum'),
     path('boards/', views.boards_index, name='index'),
+    path('posts/create/', views.PostCreate.as_view(), name='posts_create'),
+    path('posts/<int:perskey>/update/', views.PostUpdate.as_view(), name='posts_update'),
+    path('posts/<int:perskey>/delete/', views.PostDelete.as_view(), name='posts_delete'),
     # path('posts/<int:post_id>', views.post_detail, name='detail')
 ]
