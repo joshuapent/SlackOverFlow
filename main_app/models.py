21e465afb8d69853d3372883ac698b065ce44ff9
joshuapent <joshdavidpent@gmail.com>
2023-04-05 15:15:31 -0500

    made a change

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import date
 
 # Create your models here.
 
@@ -27,12 +28,13 @@ class Board(models.Model):
         return reverse('board_detail', kwargs={'board_id': self.id})
 
 class Post(models.Model):
-    upvotes = models.IntegerField(default=0)
     resolved = models.BooleanField(default=False)
     title = models.TextField(max_length=100)
     text = models.TextField(max_length=1000)
     board = models.ForeignKey(Board, on_delete=models.CASCADE, default=1)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
 
     def __str__ (self):
         return f'{self.user.name}: ({self.title})'
@@ -41,13 +43,21 @@ class Post(models.Model):
         return reverse('post_detail', kwargs={'post_id': self.id})
 
 class Comment(models.Model):
-    upvotes = models.IntegerField(default=0)
     text = models.TextField(max_length=1000)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
 
     def __str__ (self):
         return f'{self.user}, {self.post.title}'
     
     def get_absolute_url (self):
         return reverse('post_detail', kwargs={'post_id': self.post.id})
+
+class Upvotes(models.Model):
+    post = models.OneToOneField(Post, related_name='upvotes', on_delete=models.CASCADE)
+    users = models.ManyToManyField(User)
+
+    def __str__ (self):
+        return f"Like connected to: {self.post.title}"
