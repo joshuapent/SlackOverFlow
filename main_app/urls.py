7977a98d0e95f9d74d5b05ce9406438b1de48d13
joshuapent <joshdavidpent@gmail.com>
2023-04-06 13:50:19 -0500

    doing this for the dude himself

--- main_app/urls.py
+++ main_app/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('user/<int:user_id>/update/', views.user_edit, name='user_edit'),
     path('user/<int:pk>/update_account/', views.UserUpdate.as_view(), name='user_update'),
     path('user/<int:pk>/profile_update/', views.ProfileUpdate.as_view(), name='profile_update'),
-    path('change-password/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html', success_url = '/accounts/password_change/done/'), name='change_password')
+    path('change-password/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html', success_url = '/accounts/password_change/done/'), name='change_password'),
+    path('posts/<int:post_id>/upvote', views.upvote, name='upvote'),
 ] 
