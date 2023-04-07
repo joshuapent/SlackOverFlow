d8a3eed2c0b63f06af12aca01dd644255620a8df
guantanamobosch <luketyson17@gmail.com>
2023-04-07 14:15:21 -0600

    migrate

--- SlackOverFlow/settings.py
+++ SlackOverFlow/settings.py
@@ -128,6 +128,9 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_REDIRECT_URL = '/boards/'
 LOGOUT_REDIRECT_URL = '/'
